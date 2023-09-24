b36e26b95e3a0853b6dc6f03b7c42d0d1236cf6c
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2023-09-24 22:19:26 +0900

    feat: 実行時間変更

--- api/helloworld1/index.ts
+++ api/helloworld1/index.ts
@@ -4,10 +4,10 @@ const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    await sleep(10000); // 10s
+    await sleep(180000); // 3min
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: "hello world"
+        body: "hello world 1"
     };
 };
 
