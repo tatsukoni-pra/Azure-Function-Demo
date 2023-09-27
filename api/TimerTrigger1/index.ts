082778382203e5f95997d84fa9c2bb16ae105bbc
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2023-09-27 23:42:56 +0900

    add: 定時実行関数v2デプロイ

--- api/TimerTrigger1/index.ts
+++ api/TimerTrigger1/index.ts
@@ -1,15 +1,15 @@
 import { AzureFunction, Context } from "@azure/functions"
 
 const timerTrigger: AzureFunction = async function (context: Context, myTimer: any): Promise<void> {
-    context.log('Start timer trigger function');
+    context.log('Start timer trigger function v2');
 
     var timeStamp = new Date().toISOString();
     if (myTimer.isPastDue) {
-        context.log('Timer function is running late!');
+        context.log('Timer function v2 is running late!');
     }
-    context.log('Timer trigger function ran!', timeStamp);
+    context.log('Timer trigger function v2 ran!', timeStamp);
 
-    context.log('Finish timer trigger function');
+    context.log('Finish timer trigger function v2');
 };
 
 export default timerTrigger;
