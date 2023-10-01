6bc210b82b2fea0a0ccca5aca73006440f63442f
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2023-10-01 14:09:56 +0900

    add: DB更新実装

--- api/TimerTrigger1/index.ts
+++ api/TimerTrigger1/index.ts
@@ -1,4 +1,7 @@
+import { CosmosClient, PatchOperation } from "@azure/cosmos";
 import { AzureFunction, Context } from "@azure/functions"
+
+const client = new CosmosClient(process.env["COSMOSDB_CONNECTION_STRING"]);
 
 const timerTrigger: AzureFunction = async function (context: Context, myTimer: any): Promise<void> {
     context.log('Start timer trigger function v2');
@@ -7,7 +10,16 @@ const timerTrigger: AzureFunction = async function (context: Context, myTimer: a
     if (myTimer.isPastDue) {
         context.log('Timer function v2 is running late!');
     }
-    context.log('Timer trigger function v2 ran!', timeStamp);
+
+    const container = client.database("TatsukoniTest").container("tatsukoni-test-1");
+    const operations: PatchOperation[] = [
+        {
+          op: "replace", // replace operation を使用
+          path: "/text", // 更新するプロパティのパス
+          value: "Timer trigger function v1 run! " + timeStamp
+        }
+    ];
+    const { resource: updateItem } = await container.item("1", "1").patch(operations);
 
     context.log('Finish timer trigger function v2');
 };
