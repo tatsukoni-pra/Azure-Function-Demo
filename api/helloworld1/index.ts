351b730842eeba5b3c04dfce871b71678aa124fe
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2023-10-01 13:32:46 +0900

    add: DB疎通実装追加

--- api/helloworld1/index.ts
+++ api/helloworld1/index.ts
@@ -1,13 +1,19 @@
+import { CosmosClient } from "@azure/cosmos";
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
-const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
+// const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+const client = new CosmosClient(process.env["COSMOSDB_CONNECTION_STRING"]);
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    await sleep(180000); // 3min
+
+    const container = client.database("TatsukoniTest").container("tatsukoni-test-1");
+    const { resource: item } = await container.item("1", "1").read();
+
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: "hello world v3"
+        body: item
     };
 };
 
