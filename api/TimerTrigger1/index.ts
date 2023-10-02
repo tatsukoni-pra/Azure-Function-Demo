e5d22a5b0ac73b2ffece5fbebbbae2dbd55a3407
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2023-10-03 00:56:50 +0900

    feat: 定時実行調整

--- api/TimerTrigger1/index.ts
+++ api/TimerTrigger1/index.ts
@@ -4,11 +4,6 @@ import { AzureFunction, Context } from "@azure/functions"
 const client = new CosmosClient(process.env["COSMOSDB_CONNECTION_STRING"]);
 
 const timerTrigger: AzureFunction = async function (context: Context, myTimer: any): Promise<void> {
-    if (process.env.IS_SLOT && process.env.IS_SLOT === "true") {
-        context.log('This is Slot. Skip timer trigger function');
-        return;
-    }
-
     context.log('Start timer trigger function');
 
     var timeStamp = new Date().toISOString();
@@ -21,10 +16,10 @@ const timerTrigger: AzureFunction = async function (context: Context, myTimer: a
         {
             op: "replace", // replace operation を使用
             path: "/text", // 更新するプロパティのパス
-            value: "Timer trigger function v1 run! " + timeStamp
+            value: "2023/10/03 Timer trigger function run! " + timeStamp
         }
     ];
-    const { resource: updateItem } = await container.item("2", "2").patch(operations);
+    const { resource: updateItem } = await container.item("1", "1").patch(operations);
 
     context.log('Finish timer trigger function v2');
 };
