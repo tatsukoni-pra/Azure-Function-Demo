64a39b6e1ca22ba42e6cc951595d5d26cab002d4
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2023-10-03 08:07:49 +0900

    feat: 検証

--- api/helloworld1/index.ts
+++ api/helloworld1/index.ts
@@ -1,15 +1,17 @@
 import { CosmosClient } from "@azure/cosmos";
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
-// const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
+const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
 const client = new CosmosClient(process.env["COSMOSDB_CONNECTION_STRING"]);
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
 
+    await sleep(180000);
+
     const container = client.database("TatsukoniTest").container("tatsukoni-test-1");
-    const { resource: item } = await container.item("2", "2").read();
+    const { resource: item } = await container.item("1", "1").read();
 
     context.res = {
         // status: 200, /* Defaults to 200 */
